4a0eb41496310a50b4baa3f1a130541487f95774
DVD <zjdavid.2003@gmail.com>
2024-11-07 14:21:38 -0500

    Initial Commit

---
+++ TinyTactile/TinyTactileApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyTactileApp.swift
+//  TinyTactile
+//
+//  Created by dvd on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct TinyTactileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TinyTactileTests/TinyTactileTests.swift
@@ -0,0 +1,17 @@
+//
+//  TinyTactileTests.swift
+//  TinyTactileTests
+//
+//  Created by dvd on 11/7/24.
+//
+
+import Testing
+@testable import TinyTactile
+
+struct TinyTactileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
