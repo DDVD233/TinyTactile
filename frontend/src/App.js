566aa5a84596b03415a3ec8e71c0f166e3222116
Gary Nguyen <garynguyen2018@gmail.com>
2024-11-24 16:58:17 -0500

    Text excerpt and text input

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from 'react';
 
 function App() {
+  
+  const inputRef= useRef(null);
+
+  const [text, setText] = useState("Init text");
+  const [userInput, setUserInput] = useState("");
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          marginTop: "10em"
+        }} 
+    >
+      <div
+        style={{
+          width: "60%",
+          fontSize: "2rem"
+        }}
+      >
+        {text}
+      </div>
+      <input
+        type='text'
+        ref={inputRef}
+        value={userInput}
+        onChange={(e) => setUserInput(e.target.value)}
+        style={{
+          width: "60%",
+          marginTop: "2.5em",
+          borderRadius: "0.5rem",
+          border: "0.15rem solid black",
+          fontSize: "2rem"
+        }}
+      />
     </div>
   );
 }
